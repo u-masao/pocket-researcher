b922c30f176f907cf3d7ab678e982c377a877ca6
u-masao <4973920+u-masao@users.noreply.github.com>
2025-03-28 21:40:59 +0900

    入力確認ループを修正

--- src/pocket_researcher/manager.py
+++ src/pocket_researcher/manager.py
@@ -17,13 +17,6 @@ USAGE = """
 """
 
 
-def read_multiple_input() -> str:
-    """標準入力を読み取る関数"""
-    print("    (入力終了は Ctrl+D または Ctrl+Z を入力)")
-    query = sys.stdin.readlines()
-    return "".join(query)
-
-
 class PocketResearchManager:
     """
     デモ用の最小限のリサーチマネージャー。
@@ -31,40 +24,62 @@ class PocketResearchManager:
 
     def __init__(self) -> None:
         self.logger = logging.getLogger(__name__)
-        logging.basicConfig(level=logging.WARNING)
+        # logging.basicConfig(level=logging.WARNING)
 
     async def run(self) -> None:
-        # print usage
         print(textwrap.dedent(USAGE))
 
         with trace("リサーチサービス", trace_id=gen_trace_id()):
+            query = await self._input_background_and_objective_draft()
+            print(f"背景と目的のドラフトが確定しました。\n\n```\n{query}\n```")
 
-            conferm_background_and_objective = False
-            while not conferm_background_and_objective:
-                # 背景と目的のラフを入力
-                print(">>> 考えたいことや調べたいことの背景と目的を書いて:")
-                query = read_multiple_input()
-                print(f">>> 背景と目的:\n\n```\n{query}\n```")
-                print(">>> 回答作成中")
-
-                # レビュー生成
-                result = await Runner.run(
-                    background_objective_maker_agent,
-                    input=query,
-                )
-                print(
-                    f">>> 背景と目的と指摘事項:\n\n```\n{result.final_output}\n```"
-                )
-
-                # 人間に確認
-                while True:
-                    command = input(
-                        ">>> 背景と目的を清書しますか？(yes/no)"
-                    ).lower()
-                    if command == "yes":
-                        conferm_background_and_objective = True
-                    elif len(command) > 0:
-                        continue
-
-        print(f"背景と目的が確定しました。\n\n```\n{query}\n```")
         print("Research completed.")
+
+    async def _read_multiple_input(self) -> str:
+        """標準入力を読み取る関数"""
+        print("    (入力終了は Ctrl+D または Ctrl+Z を入力)")
+        query = sys.stdin.readlines()
+        return "".join(query)
+
+    async def _input_background_and_objective_draft(self):
+
+        # 終了フラグ
+        conferm_background_and_objective = False
+
+        # 終了フラグが立つまでループ
+        while not conferm_background_and_objective:
+
+            # 背景と目的のラフを入力
+            print(">>> 考えたいことや調べたいことの背景と目的を書いて:")
+            query = await self._read_multiple_input()
+            print(f">>> 背景と目的:\n\n```\n{query}\n```")
+            print(">>> 回答作成中")
+
+            # レビュー生成
+            result = await Runner.run(
+                background_objective_maker_agent,
+                input=query,
+            )
+            print(
+                f">>> 背景と目的と指摘事項:\n\n```\n{result.final_output}\n```"
+            )
+
+            # 人間に確認するループ
+            while True:
+                command = input(
+                    ">>> 背景と目的を清書しますか？(yes/no)"
+                ).lower()
+
+                # 入力チェック
+                if len(command) == 0 or command not in ["yes", "no"]:
+                    continue
+
+                # yes で終了フラグを立てる
+                if command == "yes":
+                    conferm_background_and_objective = True
+
+                # 人間に確認するループを終了
+                break
+
+        # 最後にユーザーが入力した文章を返す
+        return query

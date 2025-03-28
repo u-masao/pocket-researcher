18730e7f134c3d56c8904642305f2d22ad08b6f8
u-masao <4973920+u-masao@users.noreply.github.com>
2025-03-28 21:22:51 +0900

    make input flow

--- src/pocket_researcher/main.py
+++ src/pocket_researcher/main.py
@@ -1,29 +1,13 @@
 import asyncio
-import sys
 
 from .manager import PocketResearchManager
 
 # ポケットリサーチャーの例のエントリーポイント。
-# `python -m src.pocket-researcher.main` として実行し、
-# リサーチクエリを入力します。例えば：
-# "再生可能エネルギー投資の最近の動向を分析してください。"
-
-
-USAGE = """
-========================================================
-このアプリケーションはあなたのリサーチを助けるものです。
-考えたいことや調べたいことを入力することで、論文の形式で
-リサーチ結果を返します。
-========================================================
-"""
 
 
 async def main() -> None:
-    print(USAGE)
-    print("考えたいことや調べたいことの背景と目的を書いて:")
-    query = sys.stdin.readlines()
     mgr = PocketResearchManager()
-    await mgr.run("\n".join(query))
+    await mgr.run()
 
 
 if __name__ == "__main__":
--- src/pocket_researcher/manager.py
+++ src/pocket_researcher/manager.py
@@ -1,10 +1,27 @@
 import logging
+import sys
+import textwrap
 
 from agents import Runner, gen_trace_id, trace
 
 from pocket_researcher.agents.background_objective_maker_agent import (
     background_objective_maker_agent,
 )
+
+USAGE = """
+    ========================================================
+    このアプリケーションはあなたのリサーチを助けるものです。
+    考えたいことや調べたいことを入力することで、論文の形式で
+    リサーチ結果を返します。
+    ========================================================
+"""
+
+
+def read_multiple_input() -> str:
+    """標準入力を読み取る関数"""
+    print("    (入力終了は Ctrl+D または Ctrl+Z を入力)")
+    query = sys.stdin.readlines()
+    return "".join(query)
 
 
 class PocketResearchManager:
@@ -14,19 +31,40 @@ class PocketResearchManager:
 
     def __init__(self) -> None:
         self.logger = logging.getLogger(__name__)
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.WARNING)
 
-    async def run(self, query: str) -> None:
-        self.logger.info(f"Running research for query: {query}")
+    async def run(self) -> None:
+        # print usage
+        print(textwrap.dedent(USAGE))
 
         with trace("リサーチサービス", trace_id=gen_trace_id()):
 
-            result = await Runner.run(
-                background_objective_maker_agent,
-                input=query,
-            )
+            conferm_background_and_objective = False
+            while not conferm_background_and_objective:
+                # 背景と目的のラフを入力
+                print(">>> 考えたいことや調べたいことの背景と目的を書いて:")
+                query = read_multiple_input()
+                print(f">>> 背景と目的:\n\n```\n{query}\n```")
+                print(">>> 回答作成中")
 
-            self.logger.info(
-                "背景と目的と指摘事項:\n" f"{result.final_output}"
-            )
-        self.logger.info("Research completed.")
+                # レビュー生成
+                result = await Runner.run(
+                    background_objective_maker_agent,
+                    input=query,
+                )
+                print(
+                    f">>> 背景と目的と指摘事項:\n\n```\n{result.final_output}\n```"
+                )
+
+                # 人間に確認
+                while True:
+                    command = input(
+                        ">>> 背景と目的を清書しますか？(yes/no)"
+                    ).lower()
+                    if command == "yes":
+                        conferm_background_and_objective = True
+                    elif len(command) > 0:
+                        continue
+
+        print(f"背景と目的が確定しました。\n\n```\n{query}\n```")
+        print("Research completed.")
